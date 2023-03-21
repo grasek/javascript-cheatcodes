43b0c95b2c39c2f413ebe1687e8530f6037b3159
grasek <121408737+grasek@users.noreply.github.com>
2023-03-21 12:44:17 +0100

    Update alphabeticalValues.js

--- javascript-cheatcodes/cheatcodes/alphabeticalValues.js
+++ javascript-cheatcodes/cheatcodes/alphabeticalValues.js
@@ -1,6 +1,5 @@
 export const alphabeticalValues = () => {
-    let dictionary = {};
-    let val = 1;
+    let dictionary = {}, val = 1;
     for (let i = 97; i <= 122; i++) {
         let key = String.fromCharCode(i);
         dictionary[key] = val;
