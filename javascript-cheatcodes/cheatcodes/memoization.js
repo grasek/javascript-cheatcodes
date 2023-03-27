a279e9f4e564c0f69c5c1a8fd63678df53064abc
grasek <121408737+grasek@users.noreply.github.com>
2023-03-27 16:59:54 +0200

    Create memoization.js

---
+++ javascript-cheatcodes/cheatcodes/memoization.js
@@ -0,0 +1,12 @@
+export function memoize(func) {
+   const cache = {};
+   return function (...args) {
+      const key = JSON.stringify(args);
+
+      if (cache[key]) return cache[key];
+
+      const result = func.apply(this, args);
+      cache[key] = result;
+      return result;
+   };
+}
