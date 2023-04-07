425363dd4fcb48013ae689ccba5f8848f4c4b5ac
grasek <121408737+grasek@users.noreply.github.com>
2023-04-07 16:44:24 +0200

    Update areConsecutive.js

--- javascript-cheatcodes/cheatcodes/areConsecutive.js
+++ javascript-cheatcodes/cheatcodes/areConsecutive.js
@@ -1,9 +1,7 @@
 export const areConsecutive = (arr1, arr2) => {
    arr1.sort((a, b) => a - b);
    const start = arr2.findIndex(
-      (x, i) =>
-         i + arr1.length <= arr2.length &&
-         arr1.every((y, j) => y === arr2[i + j])
+      (_, i) => i + arr1.length <= arr2.length && arr1.every((y, j) => y === arr2[i + j])
    );
    return start !== -1;
 };
