9c618b42fb2c9ab7ba146a617d1d78df83c190fa
grasek <121408737+grasek@users.noreply.github.com>
2023-04-20 12:46:15 +0200

    Update charInString.js

--- javascript-cheatcodes/cheatcodes/charInString.js
+++ javascript-cheatcodes/cheatcodes/charInString.js
@@ -1,4 +1,1 @@
-export const charInString = (str) => {
-    // currently counted letter is "x", "g" stands for "global", it means this regEx is searching the whole input, without stopping after the first match
-    return (str.match(/x/g) || []).length
-};
+export const charInString = (str) => (str.match(/x/g) || []).length;
